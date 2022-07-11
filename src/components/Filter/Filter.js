78a915b0c4c840061d2b2e9a4f276fca456f1029
Iryusha <Sherbinina_Irina@ukr.net>
2022-07-11 22:50:50 +0300

    task7

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+import s from './Filter.module.css';
+// import { filterContacts } from '../../redux/Phonebook/phoonebook-actions';
+import { filterContacts } from 'redux/phonebookSlice';
+
+const Filter = () => {
+  const filter = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
+  return (
+    <label className={s.label} htmlFor="">
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        value={filter}
+        onChange={e => dispatch(filterContacts(e.target.value))}
+      />
+    </label>
+  );
+};
+export default Filter;
